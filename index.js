b30db0cf7466c59792e44b93f68ef25e2d70ebaa
iyabz <49148715+iyabz@users.noreply.github.com>
2020-03-27 18:09:42 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+let name = "Iyabode Atoyebi";
+let courses = ["HTML", "CSS", "JAVASCRIPT"];
+console.log("Hello World! My name is " + name + ".");
+console.log("I offer these courses: " + courses);
+
+for (i = 1; i <= 200; i++) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
